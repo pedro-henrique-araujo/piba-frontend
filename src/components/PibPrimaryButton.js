9de59e570097e503a52d2cff413751dab7fdc8da
Pedro <pedrohenriqueribeirodearaujo01@gmail.com>
2024-01-06 00:02:07 -0300

    update primary button font weight

--- src/components/PibPrimaryButton.js
+++ src/components/PibPrimaryButton.js
@@ -2,7 +2,7 @@ function PibPrimaryButton({ children, onClick, disabled }) {
   return (
     <div className="my-8">
       <button
-        className="cursor-pointer w-full bg-primary text-white font-bold rounded-lg h-10 disabled:opacity-50"
+        className="cursor-pointer w-full bg-primary text-white font-semibold rounded-lg h-10 disabled:opacity-50"
         onClick={onClick}
         disabled={disabled}
       >
