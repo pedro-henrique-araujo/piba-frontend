3baf3a4bf0a9e561d4e1813d596c747e14563ce1
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2025-05-23 20:04:17 -0300

    menu permissions

--- src/screens/menu/Menu.js
+++ src/screens/menu/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import PibPrimaryButton from "../../components/PibPrimaryButton";
+import { useAuth } from "../../AuthProvider";
 
 const Menu = () => {
   const navigate = useNavigate();
@@ -9,49 +10,56 @@ const Menu = () => {
     navigate(path);
   };
 
+  const { includesAnyRoles } = useAuth();
+
   return (
     <div className="mx-auto p-5 max-w-xl">
       <h1 className="font-bold text-2xl">Menu</h1>
       <div className="mt-5">
-        <div className="mb-5">
-          <PibPrimaryButton onClick={() => handleNavigation("/frequencia/eb")}>
-            Frequência Escola Bíblica
-          </PibPrimaryButton>
-        </div>
+        {includesAnyRoles("cante") && (
+          <div className="mb-5">
+            <PibPrimaryButton
+              onClick={() => handleNavigation("/calendario/cante")}
+            >
+              Calendário Cante
+            </PibPrimaryButton>
+          </div>
+        )}
 
-        <div className="mb-5">
-          <PibPrimaryButton
-            onClick={() => handleNavigation("/calendario/cante")}
-          >
-            Calendário Cante
-          </PibPrimaryButton>
-        </div>
+        {includesAnyRoles("media") && (
+          <div className="mb-5">
+            <PibPrimaryButton
+              onClick={() => handleNavigation("/calendario/midia")}
+            >
+              Calendário da Mídia
+            </PibPrimaryButton>
+          </div>
+        )}
 
-        <div className="mb-5">
-          <PibPrimaryButton
-            onClick={() => handleNavigation("/calendario/midia")}
-          >
-            Calendário da Mídia
-          </PibPrimaryButton>
-        </div>
-        <div className="mb-5">
-          <PibPrimaryButton onClick={() => handleNavigation("/musica")}>
-            Músicas
-          </PibPrimaryButton>
-        </div>
+        {includesAnyRoles("cante", "media") && (
+          <div className="mb-5">
+            <PibPrimaryButton onClick={() => handleNavigation("/musica")}>
+              Músicas
+            </PibPrimaryButton>
+          </div>
+        )}
 
-        <div className="mb-5">
-          <PibPrimaryButton
-            onClick={() => handleNavigation("/frequencia/sessao")}
-          >
-            Frequência Sessão
-          </PibPrimaryButton>
-        </div>
-        <div className="mb-5">
-          <PibPrimaryButton onClick={() => handleNavigation("/usuario")}>
-            Usuários
-          </PibPrimaryButton>
-        </div>
+        {includesAnyRoles("admin") && (
+          <div className="mb-5">
+            <PibPrimaryButton
+              onClick={() => handleNavigation("/frequencia/sessao")}
+            >
+              Frequência Sessão
+            </PibPrimaryButton>
+          </div>
+        )}
+        {includesAnyRoles("admin") && (
+          <div className="mb-5">
+            <PibPrimaryButton onClick={() => handleNavigation("/usuario")}>
+              Usuários
+            </PibPrimaryButton>
+          </div>
+        )}
       </div>
     </div>
   );
