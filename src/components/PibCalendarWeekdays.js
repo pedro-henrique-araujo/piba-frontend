d878a44d974619afaa6e49eb0d92d675224dfcee
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2025-12-15 23:54:00 -0300

    simplify weekdays

--- src/components/PibCalendarWeekdays.js
+++ src/components/PibCalendarWeekdays.js
@@ -1,13 +1,13 @@
 function PibCalendarWeekdays() {
   return (
     <div className="flex mt-5 text-[10px] text-gray-500 font-medium text-center">
-      <div className="w-[14%]">DOM</div>
-      <div className="w-[14%]">SEG</div>
-      <div className="w-[14%]">TER</div>
-      <div className="w-[14%]">QUA</div>
-      <div className="w-[14%]">QUI</div>
-      <div className="w-[14%]">SEX</div>
-      <div className="w-[14%]">SAB</div>
+      <div className="w-[14%]">DO</div>
+      <div className="w-[14%]">SE</div>
+      <div className="w-[14%]">TE</div>
+      <div className="w-[14%]">QU</div>
+      <div className="w-[14%]">QU</div>
+      <div className="w-[14%]">SE</div>
+      <div className="w-[14%]">SA</div>
     </div>
   );
 }
